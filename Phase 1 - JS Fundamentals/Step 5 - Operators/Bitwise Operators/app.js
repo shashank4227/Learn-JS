3c660e27eb76a13f892c3fff2263fdd9b6f6d491
Shashank <shashank4227@gmail.com>
2026-06-20 11:05:19 +0530

    Updated Operators

---
+++ Phase 1 - JS Fundamentals/Step 5 - Operators/Bitwise Operators/app.js
@@ -0,0 +1,12 @@
+let a = 1;
+let b = 2;
+
+console.log(a & b); // bitwise AND
+console.log(a | b); // bitwise OR
+console.log(a ^ b); // bitwise XOR
+
+console.log(~a); // bitwise NOT
+console.log(~b);
+
+console.log(a << b); // left shift
+console.log(a >> b); // right shift
---
+++ Phase 1 - JS Fundamentals/Step 5 - Operators/Logical Operators/app.js
@@ -0,0 +1,15 @@
+let a = true;
+let b = false;
+let c = true;
+
+console.log(a && b);
+console.log(a && c);
+console.log(b && c);
+
+console.log(a || b);
+console.log(a || c);
+console.log(b || c);
+
+console.log(!a);
+console.log(!b);
+console.log(!c);
